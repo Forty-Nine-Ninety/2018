55d8cf449a946d462ffd6ffc03f97cc7ecb4d956
gryphonrobotics <gryphonrobotics@csus.org>
2018-01-17 16:04:24 -0800

    Commited Still-Poorly-Organized Code
    Cut me some slack Dominic it's a proto-proto-proto-proto-proto-proto-proto-proto-proto-proto-proto-proto-proto-proto-proto-proto-proto-proto-proto-proto-proto-proto-proto-proto-proto-proto-proto-proto-proto-proto-proto-proto-proto-proto-proto-proto-proto-proto-proto-proto-proto-prototype

--- src/org/usfirst/frc/team4990/robot/Robot.java
+++ src/org/usfirst/frc/team4990/robot/Robot.java
@@ -18,12 +18,12 @@ public class Robot extends IterativeRobot {
 	private Encoder encoderRight, encoderLeft;
 	
 	//Digital IO Pins
-	private int encoderChannel1L = 2;
-	private int encoderChannel2L = 3;
-	private int encoderChannel1R = 0;
-	private int encoderChannel2R = 1;
+	private int encoderChannel1L = 3;
+	private int encoderChannel2L = 2;
+	private int encoderChannel1R = 1;
+	private int encoderChannel2R = 0;
 	
-	private double distance = 3, velocity = 0.95;//Distance in feet, velocity is motor power percentage, from 0-1, motor speed 12 ft/s
+	private double distance = 3, velocity = 0.25;//Distance in feet, velocity is motor power percentage, from 0-1, motor speed 12 ft/s
 	private double distanceTraveled = 0;
 	
 	//Motors
@@ -52,10 +52,10 @@ public class Robot extends IterativeRobot {
 			e.printStackTrace();
 			return;
 		}
-		motor1L = new Sad(2);
-		motor2L = new Sad(3);
-		motor1R = new Sad(0);
-		motor2R = new Sad(1);
+		motor1L = new Sad(3);
+		motor2L = new Sad(2);
+		motor1R = new Sad(1);
+		motor2R = new Sad(0);
 		
 		encoderLeft.reset();
 		encoderRight.reset();
@@ -69,26 +69,29 @@ public class Robot extends IterativeRobot {
 			double distanceTraveledDifference = encoderLeft.getDistance() - encoderRight.getDistance();
 			System.out.println(encoderLeft.getDistance() + " " + encoderRight.getDistance());
 			double correctionalVelocity = velocity;
-			/*
+			
 			if (distanceTraveledDifference < 0) {//Right is slower
 				correctionalVelocity += (distanceTraveledDifference * -1) / 144;
 			}
 			else if (distanceTraveledDifference > 0) {//Right is faster
 				correctionalVelocity -= (distanceTraveledDifference * -1) / 144;
 			}
-			*/
+			
 			motor1L.setSpeed(velocity);
 			motor2L.setSpeed(velocity);
+			correctionalVelocity *= -1;
 			motor1R.setSpeed(correctionalVelocity);
 			motor2R.setSpeed(correctionalVelocity);
 			
 			//System.out.println("Correctional: " + correctionalVelocity + " | Velocity: " + velocity + " | Distance Traveled: " + distanceTraveled + " | Distance Traveled Difference: " + distanceTraveledDifference);
 		}
-		
-		motor1L.setSpeed(0);
-		motor1R.setSpeed(0);
-		motor2L.setSpeed(0);
-		motor2R.setSpeed(0);
+		else {
+			motor1L.setSpeed(0);
+			motor1R.setSpeed(0);
+			motor2L.setSpeed(0);
+			motor2R.setSpeed(0);
+			System.out.println("HI!");
+		}
 	}
 	
 }
