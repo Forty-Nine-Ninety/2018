cd4849a31134ebf8234ae7d3751e02bd484d2183
Benjamin A <yellowjaguar5@gmail.com>
2018-01-22 16:00:49 -0800

    moto0r changes by dominic

--- src/org/usfirst/frc/team4990/robot/subsystems/motors/TalonMotorController.java
+++ src/org/usfirst/frc/team4990/robot/subsystems/motors/TalonMotorController.java
@@ -2,12 +2,12 @@ package org.usfirst.frc.team4990.robot.subsystems.motors;
 
 import edu.wpi.first.wpilibj.Talon;
 
-public class TalonMotorController extends Talon implements Motor {
+public class TalonMotorController extends Talon {
 	public TalonMotorController(int pwmPort) {
 		super(pwmPort);
 	}
 	
-	@Override
+	
 	public void setPower(double power) {
 		this.set(power);
 	}
