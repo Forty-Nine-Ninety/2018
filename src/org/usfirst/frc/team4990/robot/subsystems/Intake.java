7c2a65b2b691dd9dc039e50e2c8c5d19afae667d
MajikalExplosions <31389551+MajikalExplosions@users.noreply.github.com>
2018-01-27 15:52:06 -0800

    Added intake class

---
+++ src/org/usfirst/frc/team4990/robot/subsystems/Intake.java
@@ -0,0 +1,26 @@
+package org.usfirst.frc.team4990.robot.subsystems;
+
+//In & Out
+	// Constructor(motors)
+
+public class Intake {
+	//private Motor motor;
+	
+	/*
+	public Intake(Motor m) {
+		motor = m;
+	}
+	*/
+	
+	public void in() {
+		//Set speed
+	}
+	
+	public void out() {
+		//Set speed
+	}
+	
+	public void update() {
+		//Send info to motor
+	}
+}
