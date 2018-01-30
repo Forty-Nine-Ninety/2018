f9ccb4e59fd7549df7252f54e241022e05a79a5f
MajikalExplosions <31389551+MajikalExplosions@users.noreply.github.com>
2018-01-29 17:04:39 -0800

    Rewrote Intake Code

--- src/org/usfirst/frc/team4990/robot/subsystems/Intake.java
+++ src/org/usfirst/frc/team4990/robot/subsystems/Intake.java
@@ -1,26 +1,36 @@
 package org.usfirst.frc.team4990.robot.subsystems;
 
+import org.usfirst.frc.team4990.robot.subsystems.motors.Motor;
+
 //In & Out
 	// Constructor(motors)
 
 public class Intake {
-	//private Motor motor;
+	private Motor motor;
+	private double speed;
+	private double rate = 0.9;
 	
-	/*
+	
 	public Intake(Motor m) {
 		motor = m;
 	}
-	*/
+	
 	
 	public void in() {
 		//Set speed
+		speed = rate;
 	}
 	
 	public void out() {
 		//Set speed
+		speed = -rate;
 	}
 	
 	public void update() {
-		//Send info to motor
+		motor.setPower(speed);
+	}
+	
+	public void stop() {
+		speed = 0;
 	}
 }
