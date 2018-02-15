d229d4103894b29084778e92920f310e9586b9f5
Benjamin A <yellowjaguar5@gmail.com>
2018-02-14 18:33:30 -0800

    Added javadoc to motor subsystems.
    Whoops!

--- src/org/usfirst/frc/team4990/robot/subsystems/motors/TalonMotorController.java
+++ src/org/usfirst/frc/team4990/robot/subsystems/motors/TalonMotorController.java
@@ -3,14 +3,28 @@ package org.usfirst.frc.team4990.robot.subsystems.motors;
 import edu.wpi.first.wpilibj.Talon;
 
 public class TalonMotorController extends Talon implements Motor {
+	
+	/**
+	 * Initialize Talon.
+	 * @param pwmPort PWM port of Talon
+	 */
 	public TalonMotorController(int pwmPort) {
 		super(pwmPort);
 	}
 	
+	/**
+	 * Sets power of motor.
+	 * @param power Speed between -1 and 1
+	 */
 	
 	public void setPower(double power) {
 		this.set(power);
 	}
+	
+	/**
+	 * Gets speed last set to motor.
+	 * @return Speed between -1 and 1
+	 */
 	
 	public double getPower() {
 		return this.get();
