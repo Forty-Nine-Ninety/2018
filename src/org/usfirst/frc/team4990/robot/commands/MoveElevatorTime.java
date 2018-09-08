8a9ba46ed4ce71640fc7c2af6d810c845c0ccbab
Benjamin A <yellowjaguar5@gmail.com>
2018-09-08 12:34:42 -0700

    rewite MoveElevatorTime using TimedCommand

--- src/org/usfirst/frc/team4990/robot/commands/MoveElevatorTime.java
+++ src/org/usfirst/frc/team4990/robot/commands/MoveElevatorTime.java
@@ -1,52 +1,32 @@
 package org.usfirst.frc.team4990.robot.commands;
 
 import org.usfirst.frc.team4990.robot.RobotMap;
-import org.usfirst.frc.team4990.robot.subsystems.Elevator;
+import edu.wpi.first.wpilibj.command.TimedCommand;
 
-import edu.wpi.first.wpilibj.command.Command;
-
-public class MoveElevatorTime extends Command {
-	private boolean isFinished;
-	private long duration;
+public class MoveElevatorTime extends TimedCommand {
 	private double power;
-	private long startMillis;
-	private Elevator elevator;
 
 	/**
 	 * Moves elevator for duration.
-	 * @param time in milliseconds
+	 * @param time in seconds
 	 * @param power 0 to 1 for up, 0 to -1 for down
 	 */
 	
 	public MoveElevatorTime(double t, double power_input) {
+		super("MoveElevatorTime",t);
 		requires(RobotMap.elevator);
-		this.duration = (long) t;
 		this.power = power_input;
-		this.isFinished = false;
-		this.elevator = RobotMap.elevator;
 	}
-	/**
-	 * Clears time
-	 */
+
 	public void initialize() {
-		this.startMillis = System.currentTimeMillis();
-		System.out.println("moveElevatorTime(" + duration + ")");
-		elevator.setElevatorPower(power);
+		RobotMap.elevator.setElevatorPower(power);
 	}
-	/**
-	 * Sets isFinished to true if time is up
-	 */
-	public void execute() {
-		if (startMillis + duration <= System.currentTimeMillis()) {
-			//done waiting!
-			elevator.setElevatorPower(0);
-			this.isFinished = true;
-		}
+	
+	public void end() {
+		RobotMap.elevator.setElevatorPower(0);
 	}
-	/**
-	 * Returns whether the command is done or not
-	 */
-	public boolean isFinished() {
-		return this.isFinished;
+	
+	public void interrupted() {
+		end();
 	}
 }
