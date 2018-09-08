e8a462711b94a81855365fc0cdf76a8d650bf3c2
Benjamin A <yellowjaguar5@gmail.com>
2018-09-08 12:32:35 -0700

    rewrite gyroTurn with PIDCommand instead of command, changed to ahrs gyro instead of spi gyro

--- src/org/usfirst/frc/team4990/robot/commands/gyroTurn.java
+++ src/org/usfirst/frc/team4990/robot/commands/gyroTurn.java
@@ -3,65 +3,78 @@ package org.usfirst.frc.team4990.robot.commands;
 import org.usfirst.frc.team4990.robot.RobotMap;
 import org.usfirst.frc.team4990.robot.subsystems.DriveTrain;
 
-import edu.wpi.first.wpilibj.ADXRS450_Gyro;
-import edu.wpi.first.wpilibj.PIDOutput;
-import edu.wpi.first.wpilibj.command.Command;
+import com.kauailabs.navx.frc.AHRS;
 
-public class gyroTurn extends Command implements PIDOutput {
-	private double degrees;
-	private boolean isFinished;
-	private DriveTrain dt;
-	private ADXRS450_Gyro gyro;
-	boolean hasInitialized = false;
+import edu.wpi.first.wpilibj.command.PIDCommand;
+import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+
+public class gyroTurn extends PIDCommand {
+	AHRS ahrs = RobotMap.ahrs;
+	DriveTrain dt = RobotMap.driveTrain;
+    double kTargetAngleDegrees;
+    
+    /* The following PID Controller coefficients will need to be tuned 
+     to match the dynamics of your drive system.  Note that the      
+     SmartDashboard in Test mode has support for helping you tune    
+     controllers by displaying a form where you can enter new P, I,  
+     and D constants and test the mechanism.                         */
+    
+    static final double kP = 0.03;
+    static final double kI = 0.00;
+    static final double kD = 0.00;
+    static final double kF = 0.00;
 
 	/**
 	 * Turns left or right
 	 * @param inputDegrees Degrees to turn (Positive = right, negative = left)
 	 */
 	
-	public gyroTurn(double classdegrees) {
+	public gyroTurn(double degrees) {
+		super("TurnController", kP, kI, kD);
+		kTargetAngleDegrees = degrees;
 		requires(RobotMap.driveTrain);
-		// please note that the right encoder is backwards
-		this.dt = RobotMap.driveTrain;
-		this.degrees = classdegrees;
-		this.isFinished = false;
-		gyro = RobotMap.gyro;
 	}
 
 	public void initialize() {
-		hasInitialized = true;
-		this.gyro.reset();
-		System.out.println("gyroTurn(" + degrees + ")");
+	    this.setInputRange(-180.0f, 180.0f);
+	    this.getPIDController().setOutputRange(-1.0, 1.0);
+	    this.getPIDController().setAbsoluteTolerance(3);
+	    
+	    this.getPIDController().setContinuous(true);
+	    this.setName("DriveSystem", "RotateController");
+	    LiveWindow.add(this);
+	    
+	    this.setSetpoint(kTargetAngleDegrees);
+		ahrs.zeroYaw();
 	}
 
 	public void execute() {
 		
-		double currentDegreesTraveled = Math.abs(gyro.getAngle());
-		System.out.println("Current angle: " + this.gyro.getAngle() + "  Stopping at: " + this.degrees);
-		
-		if (currentDegreesTraveled < this.degrees) {
-			this.dt.setSpeed( //TODO change (1/90) to actual porportional value
-					(degrees > 0) ? //turning right?
-					(1/90) * (degrees - gyro.getAngle()) : -(1/90) * (degrees - gyro.getAngle()), 
-					(degrees > 0) ? //turning right?
-					-(1/90) * (degrees - gyro.getAngle()) : (1/90) * (degrees - gyro.getAngle())
-					); // left needs to go forwards, right needs to go backwards to turn right	
-		} else {
-			this.dt.setSpeed(0, 0);
-			this.isFinished = true;
-		}
+	}
+	
+	public void end() {
+		//PIDCommands automatically disable PID loops.
+	}
+	
+	public void interrupted() {
+		end();
 	}
 	
 	public boolean isFinished() {
-		return this.isFinished;
+		return this.getPIDController().onTarget();
+	}
+
+
+	@Override
+	protected double returnPIDInput() {
+		// TODO Auto-generated method stub
+		return ahrs.getAngle();
 	}
 
 	@Override
-	public void pidWrite(double output) {
-		if (!hasInitialized) {
-			return;
-		}
-		
+	protected void usePIDOutput(double output) {
+		dt.left.setSpeed(output);
+		dt.right.setSpeed(-output);
 		
 	}
 }
