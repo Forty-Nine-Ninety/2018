67889b6640c816338d1778bd0dc0f83b3dbdaa02
Benjamin A <yellowjaguar5@gmail.com>
2018-09-08 12:38:44 -0700

    Change to timed command

--- src/org/usfirst/frc/team4990/robot/commands/wait.java
+++ src/org/usfirst/frc/team4990/robot/commands/wait.java
@@ -1,43 +1,15 @@
 package org.usfirst.frc.team4990.robot.commands;
 
-import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.TimedCommand;
 
-public class wait extends Command {
-	private boolean isFinished;
-	private long duration;
-	private long startMillis;
+public class wait extends TimedCommand {
 	
 	/**
-	 * Makes robot wait
-	 * @param time Time to wait for in milliseconds
+	 * Makes robot wait.
+	 * @param time Time to wait for in seconds
 	 */
 	public wait(double t) {
-		this.duration = (long) t;
-		this.isFinished = false;
-	}
-	
-	/**
-	 * Clears time
-	 */
-	public void initialize() {
-		this.startMillis = System.currentTimeMillis();
-		System.out.println("wait(" + duration + ")");
-	}
-	
-	/**
-	 * Sets isFinished to true if time is up
-	 */
-	public void execute() {
-		if (startMillis + duration <= System.currentTimeMillis()) {
-			//done waiting!
-			this.isFinished = true;
-		}
-	}
-	
-	/**
-	 * Returns whether the command is done or not
-	 */
-	public boolean isFinished() {
-		return this.isFinished;
+		super(t);
+		System.out.println("wait(" + t + " sec)");
 	}
 }
