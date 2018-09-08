d7efaaa6c82bb3056a5b9456594dce54a11f3115
Benjamin A <yellowjaguar5@gmail.com>
2018-09-08 12:30:46 -0700

    change class of ElevatorPID from command to PIDCommand

--- src/org/usfirst/frc/team4990/robot/commands/ElevatorPID.java
+++ src/org/usfirst/frc/team4990/robot/commands/ElevatorPID.java
@@ -2,60 +2,94 @@ package org.usfirst.frc.team4990.robot.commands;
 
 import org.usfirst.frc.team4990.robot.RobotMap;
 
-import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.PIDCommand;
+import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 /**
  * Command that controls elevator in teleop
  * @author Class of '21 (created in 2018 season)
  */
-public class ElevatorPID extends Command{
+public class ElevatorPID extends PIDCommand {
 	
-	double setpoint = 4;
+	 static final double defaultSetpoint = 4; //change after testing
 
+	 /* The following PID Controller coefficients will need to be tuned 
+	    to match the dynamics of your elevator.  Note that the      
+	    SmartDashboard in Test mode has support for helping you tune    
+	    controllers by displaying a form where you can enter new P, I,  
+	    and D constants and test the mechanism.     
+	                        */
+	   static final double kP = 0.20;
+	   static final double kI = 0.00;
+	   static final double kD = 0.00;
+	   static final double kF = 0.00;
+	   
 	/**
-	 * Constructor for TeleopElevatorController
+	 * Constructor for ElevatorPID
+	 * @param setpoint setpoint for PID system (in feet?)
 	 * @author Class of '21 (created in 2018 season)
 	 */
-	public ElevatorPID() {
+	 
+	public ElevatorPID(double setpoint) {
+		super("ElevatorPID", kP, kI, kD, kF);
 		requires(RobotMap.elevator);
+		this.setSetpoint(setpoint);
 	}
 	
-	public ElevatorPID(double setpoint) {
-		this.setpoint = setpoint;
+	public ElevatorPID() {
+		super("ElevatorPID", kP, kI, kD, kF);
+		requires(RobotMap.elevator);
+		this.setSetpoint(defaultSetpoint);
 	}
 	
 	public void initalize() {
-		RobotMap.elevator.elevatorPID.setSetpoint(setpoint);
-		RobotMap.elevator.elevatorPID.enable();
+		this.setInputRange(0, 5); //minimumInput, maximumInput (in feet?) (fix maximum input)
+		this.getPIDController().setContinuous(false);; 
+		this.getPIDController().setOutputRange(-0.7, 0.7); //minimumOutput, maximumOutput (motor constraints)
+		this.getPIDController().setPercentTolerance(5);
+		
+		this.setName("Elevator", "ElevatorPIDController");    
+	    LiveWindow.add(this);
 	}
 	
 	/**
-	 * Gets input from gamepad and sets motor power
 	 * @author Class of '21 (created in 2018 season)
 	 */
 	
 	public void execute() {
-		System.out.println("Moving to " + RobotMap.elevator.elevatorPID.getSetpoint() + ", current speed: " + RobotMap.elevator.elevatorPID.get());
+		System.out.println("Moving to " + this.getSetpoint() + ", current speed: " + this.getPIDController().get());
 
 	}
 	
 	@Override
 	protected void end() {
-		RobotMap.elevator.elevatorPID.disable();
+		RobotMap.elevator.setElevatorPower(RobotMap.elevator.stopFallingSpeed);
 	}
 
 	// Called when another command which requires one or more of the same
 	// subsystems is scheduled to run
 	@Override
 	protected void interrupted() {
-		RobotMap.elevator.elevatorPID.disable();
-		RobotMap.elevator.setElevatorPower(RobotMap.elevator.stopFallingSpeed);
+		end();
+		
 	}
 	
 
 	@Override
 	protected boolean isFinished() {
 		// TODO Auto-generated method stub
-		return RobotMap.elevator.elevatorPID.onTarget();
+		return this.getPIDController().onTarget();
+	}
+
+	@Override
+	protected double returnPIDInput() {
+		// TODO Auto-generated method stub
+		return RobotMap.elevator.getEncoderDistance(); //add factor to make this return feet from bottom of elevator (or intake to ground?)
+	}
+
+	@Override
+	protected void usePIDOutput(double output) {
+		// TODO Auto-generated method stub
+		RobotMap.elevator.setElevatorPower(output);
 	}
 
 }
