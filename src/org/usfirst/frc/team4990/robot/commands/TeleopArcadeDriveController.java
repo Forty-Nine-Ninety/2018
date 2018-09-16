3ec2a487b0a72da4be2d4ad17858e10c25c610e5
Benjamin A <yellowjaguar5@gmail.com>
2018-09-16 08:44:10 -0700

    add features from DifferentialDrive (archadeDrive) to autonomus. Needs lots of testing.

--- src/org/usfirst/frc/team4990/robot/commands/TeleopArcadeDriveController.java
+++ src/org/usfirst/frc/team4990/robot/commands/TeleopArcadeDriveController.java
@@ -7,9 +7,8 @@ import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 
 /**
- * Class for controlling drivetrains. DON'T USE THIS, WORK IN PROGRESS
+ * Class for controlling drivetrains.
  * @author Class of '21 (created in 2018 season)
- * @deprecated DON'T USE THIS, WORK IN PROGRESS
  */
 public class TeleopArcadeDriveController extends Command implements PIDOutput{
 	
@@ -18,7 +17,11 @@ public class TeleopArcadeDriveController extends Command implements PIDOutput{
 	private DriveMode driveMode;
 	
 	private PIDController turnController = new PIDController(0.03, 0, 0, 0, RobotMap.ahrs, this);
-	private double turnControllerOutput = 0;
+	
+	private PIDController arcController = new PIDController(0.03, 0, 0, 0, RobotMap.ahrs, this);
+	
+    double leftMotorOutput;
+    double rightMotorOutput;
 			
 	
 	/**
@@ -29,11 +32,18 @@ public class TeleopArcadeDriveController extends Command implements PIDOutput{
 		requires(RobotMap.driveTrain);
 		
 		turnController.setSetpoint(0);
-		turnController.setContinuous(false);
+		turnController.setContinuous(true);
 		turnController.setInputRange(-180, 180);
 		turnController.setOutputRange(-0.5, 0.5);
 		turnController.setName("TeleopDrive", "turnController");
 		LiveWindow.add(turnController);
+		
+		arcController.setSetpoint(0);
+		arcController.setContinuous(true);
+		arcController.setInputRange(-180, 180);
+		arcController.setOutputRange(-0.5, 0.5);
+		arcController.setName("TeleopDrive", "arcController");
+		LiveWindow.add(arcController);
 	}
 	
 	/**
@@ -47,18 +57,27 @@ public class TeleopArcadeDriveController extends Command implements PIDOutput{
 		double turnSteepness = RobotMap.driveGamepad.getRightJoystickY();
 		
 		if (throttle != 0 && turnSteepness != 0) { //arc turn
+			if (driveMode != DriveMode.ARC) { //if mode changed
+				RobotMap.ahrs.reset();
+				arcController.enable();
+			}
+			
 			driveMode = DriveMode.ARC;
-			RobotMap.differentialDrive.arcadeDrive(throttle, -turnSteepness);
+			this.arcDrive(throttle, -turnSteepness, false);
+			RobotMap.driveTrain.setSpeed(leftMotorOutput + arcController.get(), rightMotorOutput - arcController.get());
+			
 			
 		} else if (throttle != 0 && turnSteepness == 0) { //go forward
-			if (driveMode != DriveMode.STRAIGHT) { //changed modes
+			if (driveMode != DriveMode.STRAIGHT) { //if mode changed
 				RobotMap.ahrs.reset();
 				turnController.enable();
 			}
+			
 			driveMode = DriveMode.STRAIGHT;
-			RobotMap.driveTrain.setSpeed(throttle, turnControllerOutput);
+			this.arcDrive(throttle, turnController.get(), true);
 			
 		} else if (throttle == 0 && turnSteepness != 0) { //spin in place
+			
 			/* the right motor's velocity has the opposite sign of the the left motor's
 			 * since the right motor will spin in the opposite direction from the left
 			 */
@@ -72,18 +91,51 @@ public class TeleopArcadeDriveController extends Command implements PIDOutput{
 		
 		if (driveMode != DriveMode.STRAIGHT && turnController.isEnabled()) {
 			turnController.disable();
-			turnControllerOutput = 0;
+		}
+		
+		if (driveMode != DriveMode.ARC && arcController.isEnabled()) {
+			arcController.disable();
 		}
 
 	}
 	
 	/**
-	 * Squares the number provided and keeps sign (+ or -)
-	 * @param throttleInput number to square and keeps sign
-	 * @return squared number provided with same sign
+	 * Adapted from WPILib class DifferentialDrive, method archadeDrive
+	 * @param throttle      The robot's speed along the X axis [-1.0..1.0]. Forward is positive.
+	 * @param turnSteepness   The robot's rotation rate around the Z axis [-1.0..1.0]. Clockwise is
+	 *                    positive.
+	 * @param setSpeed Boolean, true will set motor speeds at end of function
 	 */
-	public double getSquaredThrottle(double throttleInput) {
-		return throttleInput * throttleInput * Math.signum(throttleInput);
+	
+	public void arcDrive(double throttle, double turnSteepness, boolean setSpeed) {
+		throttle = limit(throttle);
+	    turnSteepness = limit(turnSteepness);
+
+	    double maxInput = Math.copySign(Math.max(Math.abs(throttle), Math.abs(turnSteepness)), throttle);
+
+	    if (throttle >= 0.0) {
+	      // First quadrant, else second quadrant
+	      if (turnSteepness >= 0.0) {
+	        leftMotorOutput = maxInput;
+	        rightMotorOutput = throttle - turnSteepness;
+	      } else {
+	        leftMotorOutput = throttle + turnSteepness;
+	        rightMotorOutput = maxInput;
+	      }
+	    } else {
+	      // Third quadrant, else fourth quadrant
+	      if (turnSteepness >= 0.0) {
+	        leftMotorOutput = throttle + turnSteepness;
+	        rightMotorOutput = maxInput;
+	      } else {
+	        leftMotorOutput = maxInput;
+	        rightMotorOutput = throttle - turnSteepness;
+	      }
+	    }
+	    
+	    if (setSpeed) {
+	    	RobotMap.driveTrain.setSpeed(limit(leftMotorOutput), -limit(rightMotorOutput));
+	    }
 	}
 
 	@Override
@@ -103,7 +155,28 @@ public class TeleopArcadeDriveController extends Command implements PIDOutput{
 	@Override
 	public void pidWrite(double output) {
 		// TODO Auto-generated method stub
-		output = turnControllerOutput;
+	}
+	
+	/**
+	 * Squares the number provided and keeps sign (+ or -)
+	 * @param throttleInput number to square and keeps sign
+	 * @return squared number provided with same sign
+	 */
+	public double getSquaredThrottle(double value) {
+		return Math.copySign(value * value, value);
+	}
+	
+	/**
+	* Limit motor values to the -1.0 to +1.0 range.
+	*/
+	public double limit(double value) {
+	    if (value > 1.0) {
+	      return 1.0;
+	    }
+	    if (value < -1.0) {
+	      return -1.0;
+	    }
+	    return value;
 	}
 
 }
