4661f4e0a216bbcb05d6ebdab11ca93996c8f881
Benjamin A <yellowjaguar5@gmail.com>
2018-12-13 07:58:55 -0800

    Revert "Merge branch 'master' into berkshire-hathaway"
    This reverts commit fb4cc29da3f6ead167a7fb0c877f0a9414523f7c, reversing
    changes made to 16c60f3e812ac9c0a57dcc630890b24fc32440d0.

---
+++ src/main/java/org/usfirst/frc/team4990/robot/commands/wait.java
@@ -0,0 +1,15 @@
+package org.usfirst.frc.team4990.robot.commands;
+
+import edu.wpi.first.wpilibj.command.TimedCommand;
+
+public class wait extends TimedCommand {
+	
+	/**
+	 * Makes robot wait.
+	 * @param t Time to wait for in seconds
+	 */
+	public wait(double t) {
+		super(t);
+		System.out.println("wait(" + t + " sec)");
+	}
+}
